a24eed994c8ec7d7bc329382a4e8cb9b54109596
Stephen Gaffney <stephen.gaffney@yale.edu>
2019-06-21 16:18:11 +0100

    Update bokeh and app startup scripts.

--- start_strains_bokeh.sh
+++ start_strains_bokeh.sh
@@ -7,10 +7,10 @@ ROOT_DIR=$(dirname "${BASH_SOURCE}")
 source ${ROOT_DIR}/${ENV_NAME:-.env}
 
 export FLASK_ENV=${FLASK_ENV:-production}
-export FLASK_APP=${ROOT_DIR}/app.py
 PORT_BOKEH=${PORT_BOKEH:-5101}
 
-${PY_HOME}/bin/bokeh serve --port ${PORT_BOKEH} --allow-websocket-origin=${URL} \
+${PY_HOME}/bin/bokeh serve --port ${PORT_BOKEH} \
+ --allow-websocket-origin=${URL} \
  --allow-websocket-origin=${ADDRESS}:${PORT_BOKEH} \
  --allow-websocket-origin=${ADDRESS}:${PORT_APP} \
  --address ${ADDRESS} \
