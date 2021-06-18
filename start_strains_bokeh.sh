161d2d467fd76e4a9dfa438a42f87133c889d40b
Stephen Gaffney <stephen.gaffney@yale.edu>
2021-06-18 16:47:08 +0100

    Simplify bokeh server call.

--- start_strains_bokeh.sh
+++ start_strains_bokeh.sh
@@ -8,10 +8,10 @@ source ${ROOT_DIR}/${ENV_NAME:-.env}
 
 export FLASK_ENV=${FLASK_ENV:-production}
 PORT_BOKEH=${PORT_BOKEH:-5101}
+ADDRESS=${ADDRESS:-127.0.0.1}
 
 ${PY_HOME}/bin/bokeh serve --port ${PORT_BOKEH} \
- --allow-websocket-origin=${URL} \
- --allow-websocket-origin=localhost:${FLASK_RUN_PORT} \
- --allow-websocket-origin=localhost:${PORT_BOKEH} \
- --address ${ADDRESS} \
+ --allow-websocket-origin=${HOST_URL} \
+ --allow-websocket-origin=${ADDRESS}:${PORT_BOKEH} \
+ --address 127.0.0.1 \
  --show ${ROOT_DIR}/bk_server
